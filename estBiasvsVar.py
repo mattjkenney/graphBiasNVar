ae82d8f01fc8d09812e1f3983c2c5eaa75fc3a6b
mattjkenney <86480338+mattjkenney@users.noreply.github.com>
2022-12-16 22:06:05 -0500

    Add files via upload

---
+++ estBiasvsVar.py
@@ -0,0 +1,49 @@
+def main():
+
+    from matplotlib import pyplot as plt
+    from pandas import DataFrame
+    import numpy as np
+
+    def get_data(pop_size, train_size):
+        
+        # Build initial dataset
+        x = np.random.choice([0,1], size= pop_size)
+
+        # Find Population Proportions
+        df = DataFrame(x)
+        p = df.value_counts(normalize=True, dropna=True)
+        
+        # Find Sample Proportions
+        x = list(x)
+        samples = [x.pop(np.random.randint(0, len(x))) for i in range(train_size)]
+        dfs = DataFrame(samples)
+        s = df.value_counts(normalize=True, dropna=True)
+
+        return p, s
+    
+    pop_size = [1000, 2]
+    train_size_p = 0.01
+    train_size = int(train_size_p * pop_size[0])
+    Ebs = []
+    Var = []
+
+    n = 2
+    xs = [n]
+    for i in range(10):
+        n += i
+        pop_size[-1] = n
+        p, s = get_data(pop_size, train_size)
+        Ebs.append(np.mean(s.values) - np.mean(p.values))
+        Var.append(np.var(s.values))
+        xs.append(n)
+
+    xs = [i + 1 for i in range(len(Ebs))]
+    
+    plt.plot(xs, Ebs, label='Estimate Bias')
+    plt.plot(xs, Var, label='Variance')
+    plt.xticks(xs)
+    plt.xlabel('Number of Edges (n)')
+    plt.legend()
+    plt.show()
+        
+main()
